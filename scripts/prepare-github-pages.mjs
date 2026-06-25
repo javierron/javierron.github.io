c3b53be7cb143f68865e3a121db33a1cbae878ed
Javier Ron Arteaga <javierron90@gmail.com>
2026-06-25 10:24:27 +0200

    debloat
    Signed-off-by: Javier Ron Arteaga <javierron90@gmail.com>

--- scripts/prepare-github-pages.mjs
+++
@@ -1,8 +0,0 @@
-import { copyFileSync, mkdirSync, writeFileSync } from "node:fs";
-import { resolve } from "node:path";
-
-const distDir = resolve("dist", "client");
-
-mkdirSync(distDir, { recursive: true });
-copyFileSync(resolve(distDir, "index.html"), resolve(distDir, "404.html"));
-writeFileSync(resolve(distDir, ".nojekyll"), "");
